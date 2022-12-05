4081679b5c3adf37f85538bd87e04820d901d7f7
matthew.mccord <matthew.mccord@vivint.com>
2022-12-05 16:08:51 -0700

    updated

--- ISCareerExploration/ISCareerExploration/wwwroot/js/site.js
+++ ISCareerExploration/ISCareerExploration/wwwroot/js/site.js
@@ -15,29 +15,29 @@ $("#Score_Calc").click(function () {
 
    
 
-    if (document.getElementById("questionOne").value == "Tableau") {
+    if (document.getElementById("questionOneTab").checked) {
         BI += 1
     }
-    else if (document.getElementById("questionOneHTML").value == "HTML/CSS") {
+    else if (document.getElementById("questionOneHTML").checked) {
         Dev += 1
     }
-    else if (document.getElementById("questionOneVBA").value == "VBA") {
+    else if (document.getElementById("questionOneVBA").checked) {
         Dev += 1
     }
-    else if (document.getElementById("questionOneERD").value == "ERD") {
+    else if (document.getElementById("questionOneERD").checked) {
         Consulting += 1
     }
-    else if (document.getElementById("questionOneSec").value == "Security") {
+    else if (document.getElementById("questionOneSec").checked) {
         Dev += 1
     }
-    else if (document.getElementById("questionOneBI").value == "BI") {
+    else if (document.getElementById("questionOneBI").checked) {
         BI += 1
     }
     else {
         Other += 1
     }
-    console.log(document.getElementById("questionOneVBA").value)
-    console.log(document.getElementsByName("questionOne").value)
+    console.log(document.getElementById("questionOneVBA").checked)
+    console.log(document.getElementById("questionOneSec").checked)
     console.log(Management)
     console.log(Other)
     console.log(Dev)
@@ -78,364 +78,6 @@ $("#Score_Calc").click(function () {
     console.log(Dev)
     console.log(BI)
 
-<<<<<<< Updated upstream
-    //if (document.getElementById("questionFour").value == "C#, JavaScript, HTML, CSS"){
-    //    Dev += 1
-    //}
-    //else if (document.getElementById("questionFour").value == "Encryption, Password Cracking, Penetration Testing"){
-    //    Security += 0
-    //}
-    //else if (document.getElementById("questionFour").value == "Organization through Kanban"){
-    //    Management += 0
-    //}
-    //else if (document.getElementById("questionFour").value == "Statistics with Python"){
-    //    Analytics += 0
-    //}
-    //else if (document.getElementById("questionFour").value == "Salesforce"){
-    //    Consulting += 0
-    //}
-    //else if (document.getElementById("questionFour").value == "Ticketing Systems (Jira)"){
-    //    IS += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-    ////need to fix the values here
-
-    //if (document.getElementById("questionFive").value == "Unidentified Error") {
-    //    Dev += 1
-    //}
-    //else if (document.getElementById("questionFive").value == "Monitoring System"){
-    //    Security += 0
-    //}
-    //else if (document.getElementById("questionFive").value == "New project"){
-    //    Management += 0
-    //}
-    //else if (document.getElementById("questionFive").value == "Model to predict"){
-    //    Analytics += 0
-    //}
-    //else if (document.getElementById("questionFive").value == "Create a technology"){
-    //    Consulting += 0
-    //}
-    //else if (document.getElementById("questionFive").value == "Troubleshooting"){
-    //    IS += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if (document.getElementById("questionSix").value == "Designer/Creator"){
-    //    Dev += 1
-    //}
-    //else if (document.getElementById("questionSix").value == "Preventer"){
-    //    Security += 1
-    //}
-    //else if (document.getElementById("questionSix").value == "Planner"){
-    //    Management += 1
-    //}
-    //else if (document.getElementById("questionSix").value == "Leader"){
-    //    Management += 1
-    //}
-    //else if (document.getElementById("questionSix").value == "Problem Solver"){
-    //    Consulting += 1
-    //    IT += 1
-    //}
-    //else if (document.getElementById("questionSix").value == "Analytical"){
-    //    BI += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#7").val() == "More Frequent"){
-    //    Dev += 1
-    //    Consulting += 1
-    //    IT += 1
-    //}
-    //else if ($("#7").val() == "Less Frequent"){
-    //    Security += 1
-    //    BI += 1
-    //    Management += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#8").val() == "Math"){
-    //    BI += 1
-    //}
-    //else if ($("#8").val() == "Innovate"){
-    //    Dev += 1
-    //}
-    //else if ($("#8").val() == "Support"){
-    //    IT += 1
-    //}
-    //else if ($("#8").val() == "Plan"){
-    //    Management += 1
-    //}
-    //else if ($("#8").val() == "Treat"){
-    //    Security += 1
-    //}
-    //else if ($("#8").val() == "Manage"){
-    //    Management += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#9").val() == "NotInterested"){
-    //    Dev += 0
-    //}
-    //else if ($("#9").val() == "SlightlyInterested"){
-    //    Dev += 0.25
-    //}
-    //else if ($("#9").val() == "Interested"){
-    //    Dev += 0.75
-    //}
-    //else if ($("#9").val() == "ExtremelyInterested"){
-    //    Dev += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#10").val() == "NotInterested"){
-    //    Dev += 0
-    //}
-    //else if ($("#10").val() == "SlightlyInterested"){
-    //    Dev += 0.25
-    //}
-    //else if ($("#10").val() == "Interested"){
-    //    Dev += 0.75
-    //}
-    //else if ($("#10").val() == "ExtremelyInterested"){
-    //    Dev += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#11").val() == "NotInterested"){
-    //    Consulting += 0
-    //}
-    //else if ($("#11").val() == "SlightlyInterested"){
-    //    Consulting += 0.25
-    //}
-    //else if ($("#11").val() == "Interested"){
-    //    Consulting += 0.75
-    //}
-    //else if ($("#11").val() == "ExtremelyInterested"){
-    //    Consulting += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#12").val() == "NotInterested"){
-    //    Dev += 0
-    //}
-    //else if ($("#12").val() == "SlightlyInterested"){
-    //    Dev += 0.25
-    //}
-    //else if ($("#12").val() == "Interested"){
-    //    Dev += 0.75
-    //}
-    //else if ($("#12").val() == "ExtremelyInterested"){
-    //    Dev += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#13").val() == "NotInterested"){
-    //    IT += 0
-    //}
-    //else if ($("#13").val() == "SlightlyInterested"){
-    //    IT += 0.25
-    //}
-    //else if ($("#13").val() == "Interested"){
-    //    IT += 0.75
-    //}
-    //else if ($("#13").val() == "ExtremelyInterested"){
-    //    IT += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#14").val() == "NotInterested"){
-    //    Management += 0
-    //}
-    //else if ($("#14").val() == "SlightlyInterested"){
-    //    Management += 0.25
-    //}
-    //else if ($("#14").val() == "Interested"){
-    //    Management += 0.75
-    //}
-    //else if ($("#14").val() == "ExtremelyInterested"){
-    //    Management += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#15").val() == "NotInterested"){
-    //    Security += 0
-    //}
-    //else if ($("#15").val() == "SlightlyInterested"){
-    //    Security += 0.25
-    //}
-    //else if ($("#15").val() == "Interested"){
-    //    Security += 0.75
-    //}
-    //else if ($("#15").val() == "ExtremelyInterested"){
-    //    Security += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#16").val() == "NotInterested"){
-    //    Dev += 0
-    //}
-    //else if ($("#16").val() == "SlightlyInterested"){
-    //    Dev += 0.25
-    //}
-    //else if ($("#16").val() == "Interested"){
-    //    Dev += 0.75
-    //}
-    //else if ($("#16").val() == "ExtremelyInterested"){
-    //    Dev += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#17").val() == "NotInterested"){
-    //    Consulting += 0
-    //}
-    //else if ($("#17").val() == "SlightlyInterested"){
-    //    Consulting += 0.25
-    //}
-    //else if ($("#17").val() == "Interested"){
-    //    Consulting += 0.75
-    //}
-    //else if ($("#17").val() == "ExtremelyInterested"){
-    //    Consulting += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#18").val() == "NotInterested"){
-    //    BI += 0
-    //}
-    //else if ($("#18").val() == "SlightlyInterested"){
-    //    BI += 0.25
-    //}
-    //else if ($("#18").val() == "Interested"){
-    //    BI += 0.75
-    //}
-    //else if ($("#18").val() == "ExtremelyInterested"){
-    //    BI += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#19").val() == "NotInterested"){
-    //    IT += 0
-    //}
-    //else if ($("#19").val() == "SlightlyInterested"){
-    //    IT += 0.25
-    //}
-    //else if ($("#19").val() == "Interested"){
-    //    IT += 0.75
-    //}
-    //else if ($("#19").val() == "ExtremelyInterested"){
-    //    IT += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#20").val() == "True"){
-    //    BI += 1
-    //}
-    //else if ($("#20").val() == "False"){
-    //    BI += 0
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#21").val() == "True"){
-    //    Management += 1
-    //}
-    //else if ($("#21").val() == "False"){
-    //    Management += 0
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#22").val() == "True"){
-    //    BI += 1
-    //}
-    //else if ($("#22").val() == "False"){
-    //    BI += 0
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#23").val() == "True"){
-    //    Dev += 1
-    //    Security += 1
-    //}
-    //else if ($("#23").val() == "False"){
-    //    Dev += 0
-    //    Security += 1
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#24").val() == "True"){
-    //    Management += 1
-    //}
-    //else if ($("#24").val() == "False"){
-    //    Management += 0
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#25").val() == "True"){
-    //    Management += 1
-    //}
-    //else if ($("#25").val() == "False"){
-    //    Management += 0
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    //if ($("#26").val() == "True"){
-    //    BI += 1
-    //}
-    //else if ($("#26").val() == "False"){
-    //    BI += 0
-    //}
-    //else {
-    //    Other += 1
-    //}
-
-    if (document.getElementById(questionTwentySevenTrue).checked() == "True") {
-        Security += 1
-    }
-    else if ($("#27").val() == "False") {
-=======
     if (document.getElementById("questionFourC").checked){
         Dev += 1
     }
@@ -792,24 +434,12 @@ $("#Score_Calc").click(function () {
         Security += 1
     }
     else if (document.getElementById("questionTwentySevenFalse").checked) {
->>>>>>> Stashed changes
         Security += 0
     }
     else {
         Other += 1
     }
 
-<<<<<<< Updated upstream
-    //if ($("#28").val() == "True") {
-    //    Consulting += 1
-    //}
-    //else if ($("#28").val() == "False") {
-    //    Consulting += 0
-    //}
-    //else {
-    //    Other += 1
-    //}
-=======
     if (document.getElementById("questionTwentyEightTrue").checked) {
         Consulting += 1
     }
@@ -819,7 +449,6 @@ $("#Score_Calc").click(function () {
     else {
         Other += 1
     }
->>>>>>> Stashed changes
 
     if
         ((Dev > BI) &&
