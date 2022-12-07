64b869e7feba106b2a240aff2361221d84f50fc8
brinleymiller122 <miller.brinley@gmail.com>
2022-12-06 21:20:52 -0700

    pdf

--- ISCareerExploration/ISCareerExploration/wwwroot/js/site.js
+++ ISCareerExploration/ISCareerExploration/wwwroot/js/site.js
@@ -506,3 +506,33 @@ $("#Score_Calc").click(function () {
 
 //finalOutput = "Your recommendation is: " + recommendation
 //document.getElementById("Output").innerHTML = finalOutput;
+
+$(function() {
+
+  var doc = new jsPDF();
+
+  var specialElementHandlers = {
+
+    '#editor': function(element, renderer) {
+
+      return true;
+
+    }
+
+  };
+
+  $('#cmd').click(function() {
+
+      doc.fromHTML($('#content').html(), 15, 15, {
+
+      'width': 170,
+
+      'elementHandlers': specialElementHandlers
+
+    });
+
+    doc.save('sample-file.pdf');
+
+  });
+
+});
